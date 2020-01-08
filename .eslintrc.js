bda6722c1642399843fbebaa7584595830e61b93
mecab <mecab@misosi.ru>
2020-01-08 14:20:44 +0900

    Organize packages and add test scaffold

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended'
   ],
   'plugins': ['@typescript-eslint'],
