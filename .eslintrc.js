eedc1f3e56d9cd0c04122e4c7931f0deaabfecb8
mecab <mecab@misosi.ru>
2020-04-24 01:16:13 +0900

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 module.exports = {
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended'
   ],
-  'plugins': ['@typescript-eslint'],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
-  'rules': {
+  rules: {
+    'semi': 'error',
+    'curly': 'error',
+    'eqeqeq': [ 'error', 'smart' ],
+    'prefer-arrow-callback': 'error',
+    'radix': 'error',
+    'quotes': [ 'error', 'single', { avoidEscape: true } ],
+    'no-await-in-loop': 'warn',
+    'no-console': 'warn',
+    'no-template-curly-in-string': 'warn',
+    '@typescript-eslint/explicit-function-return-type': [ 'error', { allowExpressions: true } ],
+    '@typescript-eslint/no-unused-vars': [ 'warn', { argsIgnorePattern: "^_" } ]
   }
 }
